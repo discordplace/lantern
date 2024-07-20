299043cec948d0dfa70718db7b4a3fdf77e5ac80
chimpdev <skyhancloud@gmail.com>
2024-07-20 10:21:51 +0300

    Refactor syncUsers function
    Optimized code to reduce database calls

--- server/lib/utils/syncUsers.js
+++ server/lib/utils/syncUsers.js
@@ -2,26 +2,24 @@ const User = require('@/models/User');
 
 async function syncUsers() {
   const guild = client.guilds.cache.get(config.base_guild_id);
-  const members = await guild.members.fetch({ withPresences: true });
+  const members = await guild.members.fetch();
 
   const allUsers = await User.find().lean();
   const usersToCreate = members.filter(member => !allUsers.some(user => user.id === member.user.id)).map(member => member);
 
-  await Promise.all(usersToCreate.map(async member => {
-    const newUser = new User({ id: member.user.id });
-
-    await newUser.save()
-      .then(() => logger.database(`User ${member.user.tag} (${member.user.id}) has been added to the database.`))
+  if (usersToCreate.length > 0) {
+    await User.insertMany(usersToCreate.map(member => ({ id: member.user.id })))
+      .then(() => logger.database(`${usersToCreate.length} users have been added to the database.`))
       .catch(error => logger.error(error));
-  }));
+  }
 
   const usersToDelete = allUsers.filter(user => !members.some(member => user.id === member.user.id));
 
-  await Promise.all(usersToDelete.map(async user => {
-    await User.deleteOne({ id: user.id })
-      .then(() => logger.database(`User ${user.id} has been removed from the database.`))
+  if (usersToDelete.length > 0) {
+    await User.deleteMany({ id: { $in: usersToDelete.map(user => user.id) } })
+      .then(() => logger.database(`${usersToDelete.length} users have been removed from the database.`))
       .catch(error => logger.error(error));
-  }));
+  }
 }
 
 module.exports = syncUsers;
