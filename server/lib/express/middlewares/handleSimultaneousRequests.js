ec51eaaefa0bec482db0203fe08188fb00e8708b
chimpdev <skyhancloud@gmail.com>
2024-09-02 23:52:31 +0300

    Refactor handleSimultaneousRequests middleware to use promisify for acquiring lock

--- server/lib/express/middlewares/handleSimultaneousRequests.js
+++ server/lib/express/middlewares/handleSimultaneousRequests.js
@@ -1,5 +1,6 @@
 const crypto = require('node:crypto');
 const AsyncLock = require('async-lock');
+const { promisify } = require('node:util');
 
 const lock = new AsyncLock();
 
@@ -16,7 +17,7 @@ const handleSimultaneousRequests = async (request, response, next) => {
   const requestKey = generateRequestKey(request);
 
   try {
-    await lock.acquire(requestKey, async () => {
+    await promisify(lock.acquire).bind(lock)(requestKey, async () => {
       // Create a new promise that resolves when the response is finished or fails if there's an error
       await new Promise((resolve, reject) => {
         response.on('finish', resolve);
