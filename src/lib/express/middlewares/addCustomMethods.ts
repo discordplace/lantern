aaf04cd688eee41b7820e74bab2d1d508ddd18ca
Gökhan Bulut <skyhancloud@gmail.com>
2025-03-30 11:45:21 +0300

    feat(middleware): enhance IP address handling and logging in production (#41)
    * feat(middleware): enhance IP address handling and logging in production
    
    * fix: update cleanIp function to handle null input

--- src/lib/express/middlewares/addCustomMethods.ts
+++ src/lib/express/middlewares/addCustomMethods.ts
@@ -1,12 +1,29 @@
 import type { Request, Response, NextFunction } from 'express';
 
-function addCustomMethods(_request: Request, response: Response, next: NextFunction): void {
+function addCustomMethods(request: Request, response: Response, next: NextFunction): void {
   response.sendError = (message: string, statusCode: number = 500): void => {
     response.status(statusCode).json({
       success: false,
       error: message
     });
   };
+
+  if (process.env.NODE_ENV === 'production') {
+    const headersToCheck = ['cf-connecting-ip', 'x-forwarded-for'];
+
+    const ip = headersToCheck.reduce((acc, header) => acc || request.headers[header], null);
+
+    request.clientIp = cleanIp(Array.isArray(ip) ? ip[0] : ip);
+  } else {
+    request.clientIp = request.ip || request.socket.remoteAddress || 'unknown';
+  }
+
+  function cleanIp(ip: string | null) {
+    if (!ip) return 'unknown';
+    if (ip.includes(',')) return ip.split(',')[0].trim();
+
+    return ip;
+  }
 
   next();
 }
--- src/lib/express/middlewares/morganMiddleware.ts
+++ src/lib/express/middlewares/morganMiddleware.ts
@@ -1,8 +1,24 @@
+import type { Request, Response } from 'express';
 import morgan from 'morgan';
 
-const morganMiddleware = morgan(':method :url from :remote-addr | Status: :status | Response Time: :response-time ms', {
+function customMorgan(tokens: morgan.TokenIndexer, request: Request, response: Response) {
+  return [
+    tokens.status(request, response),
+    tokens.method(request, response),
+    tokens.url(request, response),
+    'from ip',
+    request.clientIp,
+    tokens['response-time'](request, response),
+    'ms'
+  ].join(' ');
+}
+
+const morganMiddleware = morgan(customMorgan, {
   stream: {
     write: (message: string) => logger.log('http', message.trim())
+  },
+  skip: request => {
+    if (request.method === 'OPTIONS') return true;
   }
 });
 
