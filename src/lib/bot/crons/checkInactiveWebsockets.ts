4576f6ed62d43d05e8ebd9e429afcc1eeef5debc
Gökhan Bulut <root@skyhan.cloud>
2025-09-11 08:35:48 +0300

    fix(crons): add check for global.ActiveSockets before execution

--- src/lib/bot/crons/checkInactiveWebsockets.ts
+++ src/lib/bot/crons/checkInactiveWebsockets.ts
@@ -4,6 +4,8 @@ import type { CronType } from '@/src/types';
 export default {
   pattern: '*/10 * * * * *',
   execute: async () => {
+    if (!global.ActiveSockets) return;
+
     for (const [id, { lastHeartbeat, instance }] of ActiveSockets.entries()) {
       if (Date.now() - lastHeartbeat > config.server.socket.heartbeat_interval) {
         disconnect(instance, id, 'Connection timed out.');
